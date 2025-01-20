fab8c818491653ac9735b76e1b55fbc0c08257c6
GameHappy <zxcv4715@gmail.com>
2025-01-20 17:29:26 +0800

    update 1

--- JSONConvert/Form1.Designer.cs
+++ JSONConvert/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace JSONConvert
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,12 +28,260 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            AddColumn = new Button();
+            Col_1 = new Label();
+            Col_2 = new Label();
+            Col_3 = new Label();
+            Col_4 = new Label();
+            Col_5 = new Label();
+            EnterBtn = new Button();
+            Col1_Txt = new TextBox();
+            Col2_Txt = new TextBox();
+            Col5_Txt = new TextBox();
+            listBox1 = new ListBox();
+            OutputBtn = new Button();
+            InputBtn = new Button();
+            Col3_Txt = new NumericUpDown();
+            Col4_Txt = new NumericUpDown();
+            listRemoveBtn = new Button();
+            editBtn = new Button();
+            prevBtn = new Button();
+            openFileDialog1 = new OpenFileDialog();
+            folderBrowserDialog1 = new FolderBrowserDialog();
+            ((System.ComponentModel.ISupportInitialize)Col3_Txt).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)Col4_Txt).BeginInit();
+            SuspendLayout();
+            // 
+            // AddColumn
+            // 
+            AddColumn.BackColor = SystemColors.Window;
+            AddColumn.Enabled = false;
+            AddColumn.Location = new Point(12, 12);
+            AddColumn.Name = "AddColumn";
+            AddColumn.Size = new Size(91, 23);
+            AddColumn.TabIndex = 0;
+            AddColumn.Text = "Add Column";
+            AddColumn.UseVisualStyleBackColor = false;
+            // 
+            // Col_1
+            // 
+            Col_1.AutoSize = true;
+            Col_1.Font = new Font("Segoe UI", 12F);
+            Col_1.Location = new Point(12, 48);
+            Col_1.Name = "Col_1";
+            Col_1.Size = new Size(52, 21);
+            Col_1.TabIndex = 1;
+            Col_1.Text = "Name";
+            // 
+            // Col_2
+            // 
+            Col_2.AutoSize = true;
+            Col_2.Font = new Font("Segoe UI", 12F);
+            Col_2.Location = new Point(12, 88);
+            Col_2.Name = "Col_2";
+            Col_2.Size = new Size(50, 21);
+            Col_2.TabIndex = 2;
+            Col_2.Text = "Detail";
+            // 
+            // Col_3
+            // 
+            Col_3.AutoSize = true;
+            Col_3.Font = new Font("Segoe UI", 12F);
+            Col_3.Location = new Point(12, 128);
+            Col_3.Name = "Col_3";
+            Col_3.Size = new Size(73, 21);
+            Col_3.TabIndex = 3;
+            Col_3.Text = "ItemType";
+            // 
+            // Col_4
+            // 
+            Col_4.AutoSize = true;
+            Col_4.Font = new Font("Segoe UI", 12F);
+            Col_4.Location = new Point(12, 168);
+            Col_4.Name = "Col_4";
+            Col_4.Size = new Size(44, 21);
+            Col_4.TabIndex = 4;
+            Col_4.Text = "Price";
+            // 
+            // Col_5
+            // 
+            Col_5.AutoSize = true;
+            Col_5.Font = new Font("Segoe UI", 12F);
+            Col_5.Location = new Point(12, 208);
+            Col_5.Name = "Col_5";
+            Col_5.Size = new Size(89, 21);
+            Col_5.TabIndex = 5;
+            Col_5.Text = "Description";
+            // 
+            // EnterBtn
+            // 
+            EnterBtn.Location = new Point(12, 267);
+            EnterBtn.Name = "EnterBtn";
+            EnterBtn.Size = new Size(52, 23);
+            EnterBtn.TabIndex = 6;
+            EnterBtn.Text = "Enter";
+            EnterBtn.UseVisualStyleBackColor = true;
+            EnterBtn.Click += EnterBtn_Click;
+            // 
+            // Col1_Txt
+            // 
+            Col1_Txt.Location = new Point(127, 46);
+            Col1_Txt.Name = "Col1_Txt";
+            Col1_Txt.Size = new Size(120, 23);
+            Col1_Txt.TabIndex = 7;
+            // 
+            // Col2_Txt
+            // 
+            Col2_Txt.Location = new Point(127, 86);
+            Col2_Txt.Name = "Col2_Txt";
+            Col2_Txt.Size = new Size(120, 23);
+            Col2_Txt.TabIndex = 8;
+            // 
+            // Col5_Txt
+            // 
+            Col5_Txt.Location = new Point(127, 206);
+            Col5_Txt.Multiline = true;
+            Col5_Txt.Name = "Col5_Txt";
+            Col5_Txt.Size = new Size(152, 84);
+            Col5_Txt.TabIndex = 11;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(310, 46);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(187, 244);
+            listBox1.TabIndex = 12;
+            listBox1.SelectedIndexChanged += listBox1_SelectedIndexChanged;
+            // 
+            // OutputBtn
+            // 
+            OutputBtn.Location = new Point(513, 267);
+            OutputBtn.Name = "OutputBtn";
+            OutputBtn.Size = new Size(75, 23);
+            OutputBtn.TabIndex = 13;
+            OutputBtn.Text = "SaveJSON";
+            OutputBtn.UseVisualStyleBackColor = true;
+            OutputBtn.Click += OutputBtn_Click;
+            // 
+            // InputBtn
+            // 
+            InputBtn.Location = new Point(513, 229);
+            InputBtn.Name = "InputBtn";
+            InputBtn.Size = new Size(75, 23);
+            InputBtn.TabIndex = 14;
+            InputBtn.Text = "LoadJSON";
+            InputBtn.UseVisualStyleBackColor = true;
+            InputBtn.Click += InputBtn_Click;
+            // 
+            // Col3_Txt
+            // 
+            Col3_Txt.Location = new Point(127, 126);
+            Col3_Txt.Maximum = new decimal(new int[] { 10, 0, 0, 0 });
+            Col3_Txt.Name = "Col3_Txt";
+            Col3_Txt.Size = new Size(120, 23);
+            Col3_Txt.TabIndex = 15;
+            Col3_Txt.TextAlign = HorizontalAlignment.Center;
+            // 
+            // Col4_Txt
+            // 
+            Col4_Txt.Increment = new decimal(new int[] { 5, 0, 0, 0 });
+            Col4_Txt.Location = new Point(127, 166);
+            Col4_Txt.Maximum = new decimal(new int[] { 10000, 0, 0, 0 });
+            Col4_Txt.Name = "Col4_Txt";
+            Col4_Txt.Size = new Size(120, 23);
+            Col4_Txt.TabIndex = 16;
+            Col4_Txt.TextAlign = HorizontalAlignment.Center;
+            // 
+            // listRemoveBtn
+            // 
+            listRemoveBtn.Location = new Point(513, 85);
+            listRemoveBtn.Name = "listRemoveBtn";
+            listRemoveBtn.Size = new Size(75, 23);
+            listRemoveBtn.TabIndex = 17;
+            listRemoveBtn.Text = "Remove";
+            listRemoveBtn.UseVisualStyleBackColor = true;
+            listRemoveBtn.Click += listRemoveBtn_Click;
+            // 
+            // editBtn
+            // 
+            editBtn.Location = new Point(513, 45);
+            editBtn.Name = "editBtn";
+            editBtn.Size = new Size(75, 23);
+            editBtn.TabIndex = 18;
+            editBtn.Text = "Edit";
+            editBtn.UseVisualStyleBackColor = true;
+            editBtn.Click += editBtn_Click;
+            // 
+            // prevBtn
+            // 
+            prevBtn.Enabled = false;
+            prevBtn.Location = new Point(69, 267);
+            prevBtn.Name = "prevBtn";
+            prevBtn.Size = new Size(52, 23);
+            prevBtn.TabIndex = 19;
+            prevBtn.Text = "Prev";
+            prevBtn.UseVisualStyleBackColor = true;
+            prevBtn.Click += prevBtn_Click;
+            // 
+            // openFileDialog1
+            // 
+            openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(600, 331);
+            Controls.Add(prevBtn);
+            Controls.Add(editBtn);
+            Controls.Add(listRemoveBtn);
+            Controls.Add(Col4_Txt);
+            Controls.Add(Col3_Txt);
+            Controls.Add(InputBtn);
+            Controls.Add(OutputBtn);
+            Controls.Add(listBox1);
+            Controls.Add(Col5_Txt);
+            Controls.Add(Col2_Txt);
+            Controls.Add(Col1_Txt);
+            Controls.Add(EnterBtn);
+            Controls.Add(Col_5);
+            Controls.Add(Col_4);
+            Controls.Add(Col_3);
+            Controls.Add(Col_2);
+            Controls.Add(Col_1);
+            Controls.Add(AddColumn);
+            Name = "MainForm";
+            Text = "JSON Convert";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)Col3_Txt).EndInit();
+            ((System.ComponentModel.ISupportInitialize)Col4_Txt).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button AddColumn;
+        private Label Col_1;
+        private Label Col_2;
+        private Label Col_3;
+        private Label Col_4;
+        private Label Col_5;
+        private Button EnterBtn;
+        private TextBox Col1_Txt;
+        private TextBox Col2_Txt;
+        private TextBox Col5_Txt;
+        private ListBox listBox1;
+        private Button OutputBtn;
+        private Button InputBtn;
+        private NumericUpDown Col3_Txt;
+        private NumericUpDown Col4_Txt;
+        private Button listRemoveBtn;
+        private Button editBtn;
+        private Button prevBtn;
+        private OpenFileDialog openFileDialog1;
+        private FolderBrowserDialog folderBrowserDialog1;
     }
 }
--- JSONConvert/Form1.cs
+++ JSONConvert/Form1.cs
@@ -1,10 +1,199 @@
-namespace JSONConvert
+﻿namespace JSONConvert
 {
-    public partial class Form1 : Form
+    using System.Xml.Linq;
+    using Newtonsoft.Json;
+    using System.IO;
+    using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
+    using System.Windows.Forms;
+
+    public partial class MainForm : Form
     {
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private List<string> _Name = new List<string>();
+        private List<string> Detail = new List<string>();
+        private List<int> ItemType = new List<int>();
+        private List<int> Price = new List<int>();
+        private List<string> Description = new List<string>();
+        private string preName, preDetail, preDescription;
+        private int preItemType, prePrice;
+        bool listEditOnly = false;
+        private void EnterBtn_Click(object sender, EventArgs e)
+        {
+
+            //string Name, string Detail, int ItemType, int Price, string Description
+            string _name = "", _detail = "", _description = "";
+            int _itemType = 0, _price = 0;
+            bool _err = false;
+            //verify input format
+            try
+            {
+                _name = Col1_Txt.Text;
+                _detail = Col2_Txt.Text;
+                _itemType = (int)Col3_Txt.Value;
+                _price = (int)Col4_Txt.Value;
+                _description = Col5_Txt.Text;
+            }
+            catch
+            {
+                _err = true;
+                MessageBox.Show("Detect other error.");
+            }
+            if (!_err)
+            {
+                string msg = "";
+                _Name.Add(_name);
+                Detail.Add(_detail);
+                ItemType.Add(_itemType);
+                Price.Add(_price);
+                Description.Add(_description);
+                //msg = $"Name:{_name},Detail:{_detail},ItemType:{_itemType},Price:{_price},Description:{_description}";
+                listBox1.Items.Add(_name);
+            }
+        }
+        private void prevBtn_Click(object sender, EventArgs e)
+        {
+            Col1_Txt.Text = preName;
+            Col2_Txt.Text = preDetail;
+            Col3_Txt.Value = preItemType;
+            Col4_Txt.Value = prePrice;
+            Col5_Txt.Text = preDescription;
+            prevBtn.Enabled = false;
+        }
+        // data work
+        private void dataRemove(int Index)
+        {
+            listBox1.Items.RemoveAt(Index);
+            _Name.RemoveAt(Index);
+            Detail.RemoveAt(Index);
+            ItemType.RemoveAt(Index);
+            Price.RemoveAt(Index);
+            Description.RemoveAt(Index);
+        }
+        private void dataClear()
+        {
+            listBox1.Items.Clear();
+            _Name.Clear();
+            Detail.Clear();
+            ItemType.Clear();
+            Price.Clear();
+            Description.Clear();
+        }
+        private void dataEdit(int index)
+        {
+            listBox1.Items[index] = Col1_Txt.Text;
+            _Name[index] = Col1_Txt.Text;
+            Detail[index] = Col2_Txt.Text;
+            ItemType[index] = (int)Col3_Txt.Value;
+            Price[index] = (int)Col4_Txt.Value;
+            Description[index] = Col5_Txt.Text;
+            listEditOnly = false;
+        }
+        // data work end
+        private void editBtn_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex == -1) return;
+            listEditOnly = true;
+            dataEdit(listBox1.SelectedIndex);
+            editBtn.Enabled = false;
+            
+        }
+        private void listRemoveBtn_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex == -1) return;
+            dataRemove(listBox1.SelectedIndex);
+        }
+
+        private void OutputBtn_Click(object sender, EventArgs e)
+        {
+
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = folderBrowserDialog1.SelectedPath;
+
+                List<DataItem> dataItems = new List<DataItem>();
+                for (int i = 0; i < _Name.Count; i++)
+                {
+                    dataItems.Add(new DataItem
+                    {
+                        Name = _Name[i],
+                        Detail = Detail[i],
+                        ItemType = ItemType[i],
+                        Price = Price[i],
+                        Description = Description[i],
+                    });
+                }
+                string json = JsonConvert.SerializeObject(dataItems, Formatting.Indented);
+
+                File.WriteAllText($"{filePath}\\Itemdatabase.json", json);
+
+                MessageBox.Show("save to Json");
+            }
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (!prevBtn.Enabled)
+            {
+                preName = Col1_Txt.Text;
+                preDetail = Col2_Txt.Text;
+                preItemType = (int)Col3_Txt.Value;
+                prePrice = (int)Col4_Txt.Value;
+                preDescription = Col5_Txt.Text;
+            }
+            int index = listBox1.SelectedIndex;
+            if (index == -1) return;
+            if (!listEditOnly)
+            {
+                Col1_Txt.Text = _Name[index];
+                Col2_Txt.Text = Detail[index];
+                Col3_Txt.Value = ItemType[index];
+                Col4_Txt.Value = Price[index];
+                Col5_Txt.Text = Description[index];
+            }
+            editBtn.Enabled = true;
+            prevBtn.Enabled = true;
+        }
+
+        private void InputBtn_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = openFileDialog1.FileName;
+                string json = File.ReadAllText(filePath);
+
+                List<DataItem> dataItems = JsonConvert.DeserializeObject<List<DataItem>>(json);
+                dataClear();
+                foreach (var item in dataItems)
+                {
+                    _Name.Add(item.Name);
+                    Detail.Add(item.Detail);
+                    ItemType.Add(item.ItemType);
+                    Price.Add(item.Price);
+                    Description.Add(item.Description);
+                    listBox1.Items.Add(item.Name);
+                }
+            }
+        }
+
+        public class DataItem
+        {
+            public string Name { get; set; }
+            public string Detail { get; set; }
+            public int ItemType { get; set; }
+            public int Price { get; set; }
+            public string Description { get; set; }
+        }
+
+
     }
 }
